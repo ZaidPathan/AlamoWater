c4fce0597a2809446d440da5361d38d8765417a9
Zaid Pathan <zaidkhanintel@gmail.com>
2018-05-19 14:24:55 +0530

    Updated for Xcode 9.3.

--- AlamoWater/build/AlamoWater.build/Debug-iphoneos/AlamoWater.build/Objects-normal/armv7/AlamoWater_vers.d
+++ AlamoWater/build/AlamoWater.build/Debug-iphoneos/AlamoWater.build/Objects-normal/armv7/AlamoWater_vers.d
@@ -1,2 +1,2 @@
 dependencies: \
-  /Users/zaidpathan/Projects/OpenSource/Alamowater_GIT/AlamoWater/build/AlamoWater.build/Debug-iphoneos/AlamoWater.build/DerivedSources/AlamoWater_vers.c
+  /Users/zaidpathan/Projects/OpenSource/Alamowater/AlamoWater/build/AlamoWater.build/Debug-iphoneos/AlamoWater.build/DerivedSources/AlamoWater_vers.c
